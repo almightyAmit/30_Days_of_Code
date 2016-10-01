6ed723e10185f1f3a15bde4b22bc6e2420a8b9a3
amitsaroj001 <amitsaroj2012@gmail.com>
2016-10-01 10:55:02 +0530

    Day 3

--- Hello/src/ConditionUse.java
+++ Hello/src/ConditionUse.java
@@ -1,9 +1,44 @@
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 public class ConditionUse {
+   
+    /*
+    Task 
+    Given an integer, , perform the following conditional actions:
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    If n is odd, print Weird
+    If n is even and in the inclusive range of 2 to 5, print Not Weird
+    If n is even and in the inclusive range of 6 to 20, print Weird
+    If n is even and greater than 20, print Not Weird
 
-	}
-
+*/
+    
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int n = scan.nextInt(); 
+      scan.close();
+      String ans="";
+          
+      // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
+      if(n%2==1){
+         ans = "Weird";
+      }
+      else{
+        if(n >= 2 && n <= 5){
+            ans = "Not Weird";
+        }
+          
+        else if(n >= 6 && n <= 20){
+            ans = "Weird";
+        }
+          
+        else ans = "Not Weird";
+      }
+      System.out.println(ans);
+   }
 }
